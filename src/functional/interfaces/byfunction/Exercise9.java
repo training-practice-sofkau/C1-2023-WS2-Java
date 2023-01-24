057867831b9dea476feb97d8d2cec47e32626727
isan-san <isan.9.9f@gmail.com>
2023-01-24 16:40:57 -0500

    Resolve Exercise9 successfully

--- src/functional/interfaces/byfunction/Exercise9.java
+++
@@ -1,8 +0,0 @@
-package functional.interfaces.byfunction;
-/*
-String replacement
-Create a function that replaces all occurrences of a given character in a string with another character, using a functional approach.
-The function should take a string, and two characters as arguments and return the modified string. Use the replace method to replace the characters.
-* */
-public class Exercise9 {
-}
