939bdd8b38dcf3d34bde1a17fdb073e8f40f1408
Difsan <difsanchezru@gmail.com>
2023-01-24 17:48:21 -0500

    Generator method was done
    For String and Integer

--- src/functional/interfaces/byfunction/exercise6/IntegerGenerator.java
+++ src/functional/interfaces/byfunction/exercise6/IntegerGenerator.java
@@ -1,4 +1,13 @@
 package functional.interfaces.byfunction.exercise6;
 
-public class IntegerGenerator {
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.function.Supplier;
+
+public class IntegerGenerator implements Supplier<Integer> {
+
+    @Override
+    public Integer get() {
+        int random = ThreadLocalRandom.current().nextInt();
+        return random;
+    }
 }
--- src/functional/interfaces/byfunction/exercise6/StringGenerator.java
+++ src/functional/interfaces/byfunction/exercise6/StringGenerator.java
@@ -1,12 +1,24 @@
 package functional.interfaces.byfunction.exercise6;
 
+import java.util.Random;
 import java.util.function.Supplier;
 
 public class StringGenerator implements Supplier<String> {
     @Override
     public String get() {
         //TO-DO: The logic in order to show a random string
+        int aLimit = 97;
+        int zlimit = 122;
+        int strLenght = 6;
+        String finalStri="";
+        Random random = new Random();
+        for (int i=0; i<=strLenght; i++){
+            int l = random.nextInt()*(aLimit-zlimit);
+            char let = (char) l;
+            finalStri += let;
+        }
 
-        return null;
+        return finalStri;
     }
+
 }
