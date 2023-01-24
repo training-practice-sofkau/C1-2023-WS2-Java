844b12be6e32920dc9b65f96b769799758bd3892
Larry Mateo Ramirez C <larry.ramirez@correounivalle.edu.co>
2023-01-24 14:35:52 -0500

    Exercise 6 finished
    Create a functional interface and two implementations

--- src/functional/interfaces/byfunction/exercise6/StringGenerator.java
+++ src/functional/interfaces/byfunction/exercise6/StringGenerator.java
@@ -1,12 +1,16 @@
 package functional.interfaces.byfunction.exercise6;
 
+import java.util.List;
 import java.util.function.Supplier;
 
 public class StringGenerator implements Supplier<String> {
     @Override
     public String get() {
         //TO-DO: The logic in order to show a random string
+        List<String> strings = List.of("Wish", "Seventeen", "Rust", "Down", "Oven", "Nine", "Hearth", "Homecoming", "One", "Wagon");
 
-        return null;
+        int num = (int)Math.ceil(Math.random() * (strings.size()) - 1);
+
+        return strings.get(num);
     }
 }
