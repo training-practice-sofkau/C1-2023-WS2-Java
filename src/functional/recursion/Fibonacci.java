dfd26d3ab1be9f6a9e67d9b89b845fcf9340c245
Juangmcz <juangui-089@hotmail.com>
2023-01-24 13:17:03 -0500

    Exercise 3 is done.

--- src/functional/recursion/Fibonacci.java
+++ src/functional/recursion/Fibonacci.java
@@ -4,10 +4,10 @@ public class Fibonacci {
     //Started numbers: 0 & 1
 
     public static int calculate(int n){
-        if( n == 0 ) return 0;
-        if( n == 1) return 1;
-
-        //TO-DO: Implement the recursion dynamic to follow in order to show the fibonnaci serie
-        return 0;
+        if( n < 2 ) {
+            return n;
+        }
+        int fibonacci = calculate(n - 1) + calculate(n - 2);
+        return fibonacci;
     }
 }
