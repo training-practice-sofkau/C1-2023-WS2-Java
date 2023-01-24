2becc82d76905140e3ff10e9b2c4542586d009ed
Larry Mateo Ramirez C <larry.ramirez@correounivalle.edu.co>
2023-01-24 14:53:55 -0500

    Exercise 8 finished
    Implements Finction<T,R> in order to create a function that transform the type from String to Integer

--- src/functional/interfaces/byfunction/Exercise8.java
+++ src/functional/interfaces/byfunction/Exercise8.java
@@ -1,5 +1,9 @@
 package functional.interfaces.byfunction;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Function;
+
 /*
 *  Transformation
 Create a class that transforms a list of elements from one type to another using a Function<T, R>.
@@ -7,4 +11,18 @@ Create a class that transforms a list of elements from one type to another using
 * Function implementations that perform different types of transformations.
 */
 public class Exercise8 {
+    public static void main(String[] args) {
+        List<String> strings = List.of("1", "2", "3", "4", "5", "6");
+
+        List<Integer> numbers = new ArrayList<>();
+
+        Function<String, Integer> stringIntegerFunction = Integer::parseInt;
+
+        for (String str : strings) {
+            numbers.add(stringIntegerFunction.apply(str));
+        }
+
+        System.out.println("Strings: " + strings + strings.get(0).getClass().getSimpleName());
+        System.out.println("Numbers: " + numbers + numbers.get(0).getClass().getSimpleName());
+    }
 }
