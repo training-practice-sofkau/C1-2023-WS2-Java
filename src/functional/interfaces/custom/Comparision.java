80f771042c405988356f85e97a1cbb1a22ebfe2c
Larry Mateo Ramirez C <larry.ramirez@correounivalle.edu.co>
2023-01-24 14:16:18 -0500

    Exercise 5 finished
    Define a comparator that recive String and Integer objects en compare its length and value respectively

--- src/functional/interfaces/custom/Comparision.java
+++ src/functional/interfaces/custom/Comparision.java
@@ -1,9 +1,32 @@
 package functional.interfaces.custom;
 
 public class Comparision implements CustomComparator{
-    @Override
+
     public int comparator(Object o1, Object o2) {
         //TO-DO: Implement the neccesary logic to acomplish the assessment. Just consider String and Integer classses
+        if(o1 instanceof String && o2 instanceof String) {
+            if (((String) o1).length() == ((String) o2).length()){
+                return 0;
+            }
+            if (((String) o1).length() > ((String) o2).length()){
+                return 1;
+            }
+            if (((String) o1).length() < ((String) o2).length()){
+                return -1;
+            }
+        }
+
+        if(o1 instanceof Integer && o2 instanceof Integer) {
+            if (o1 == o2){
+                return 0;
+            }
+            if ((Integer)o1 > (Integer)o2){
+                return 1;
+            }
+            if ((Integer)o1 < (Integer)o2){
+                return -1;
+            }
+        }
         return 0;
     }
 }
