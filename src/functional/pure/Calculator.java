c26d58aa4bff8e74c87031326aed55ff489e999d
BrianSammit <briansammit.cruz@gmail.com>
2023-01-24 18:00:59 -0500

    Fix calculator with final to follow pure functions principles

--- src/functional/pure/Calculator.java
+++ src/functional/pure/Calculator.java
@@ -2,18 +2,18 @@ package functional.pure;
 
 //Remember: The result in this exercise is to apply purity
 public class Calculator {
-    public int add(final int x, final int y) {
+    int add(final int x, final int y) {
         return x + y;
     }
-    public int sub(final int x, final int y) {
+    int sub(final int x, final int y) {
         return x - y;
     }
 
-    public int multi(final int x, final int y) {
+    int multi(final int x, final int y) {
         return  x * y;
     }
 
-    public double div(final int x, final int y) {
+    double div(final int x, final int y) {
         return  (double)x / (double)y;
     }
 }
