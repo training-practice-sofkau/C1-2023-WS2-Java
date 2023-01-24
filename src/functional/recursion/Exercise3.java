d18596987221d2730e1ee610660942aad7598b26
Julian Rojas <julianrojasgeo@gmail.com>
2023-01-24 17:32:16 -0500

    Exercise 3. Recursive fibonacci

--- src/functional/recursion/Exercise3.java
+++ src/functional/recursion/Exercise3.java
@@ -5,8 +5,13 @@ package functional.recursion;
 * Use the “final” keyword to prevent reassignment of variables. Test the function by calling it with different inputs, make sure that the results are correct.
  */
 public class Exercise3 {
+
+
     public static void main(String[] args) {
-        Fibonacci.calculate(1);
+        int fiboN1 = 4;
+        int fiboN2= 6;
+        System.out.println("The Fibonacci number "+fiboN1+ " is: "+Fibonacci.calculate(fiboN1));
+        System.out.println("The Fibonacci number "+fiboN2+ " is: "+Fibonacci.calculate(fiboN2));
 
     }
 }
