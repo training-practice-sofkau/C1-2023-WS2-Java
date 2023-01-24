ccc0bed26c1394222867084010a5b998a064d2bf
Juangmcz <juangui-089@hotmail.com>
2023-01-24 17:21:35 -0500

    Exercise 5 is done.

--- src/functional/interfaces/custom/Exercise5.java
+++ src/functional/interfaces/custom/Exercise5.java
@@ -1,4 +1,7 @@
 package functional.interfaces.custom;
+
+import java.util.List;
+
 /*
 * Create a custom functional interface that represents a comparison between two objects of the same type.
 * The interface should have a single abstract method that takes two objects of the same type and returns an integer,
@@ -10,6 +13,41 @@ package functional.interfaces.custom;
  */
 public class Exercise5 {
     public static void main(String[] args) {
+
+        // Some data example
+        List<String> strings = List.of("short","Longer string", "Another string", "Just another string");
+        List<Integer> numbers = List.of(1, 4, 666, 955, 4555, 22, 555);
+
+
+        //Creating some objects
+        String string1 = "short";
+        String string2 = "Longer string";
+        Integer integer1 = 5;
+        Integer integer2 = 4;
+
+        //Testing the comparison
+        Comparison comparator = new Comparison();
+        System.out.println(comparator.compare(string1, string2));
+        System.out.println(comparator.compare(integer1, integer2));
+
+        // Sorting Lists of Objects
+        String maxString = "";
+        for(String string : strings){
+            if (comparator.compare(string, maxString) > 0){
+                maxString = string;
+            }
+        }
+
+        Integer maxInteger = 0;
+        for(Integer number : numbers){
+            if(comparator.compare(number, maxInteger) < 0){
+                maxInteger = number;
+            }
+        }
+
+        // Greater values
+        System.out.println("The longest string is: " + maxString);
+        System.out.println("The largest number is: " + maxInteger);
 
     }
 }
