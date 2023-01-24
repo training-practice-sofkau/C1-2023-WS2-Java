a7bf01da86766f19e21ab86dec4ceb9ab8b27614
William Serna <williamsernac@hotmail.com>
2023-01-24 15:26:06 -0500

    Update Exercise12.java
    Filtering exercise done

--- src/functional/interfaces/byfunction/Exercise12.java
+++ src/functional/interfaces/byfunction/Exercise12.java
@@ -1,8 +1,19 @@
 package functional.interfaces.byfunction;
+
+import java.util.function.Function;
+
 /*
 * Number Comparison
 Create a function that compares two integers using a functional approach. The function should take two integers as arguments,
 * and return a boolean indicating whether the first integer is greater than the second. Use the > operator to compare the two input integers.
 */
 public class Exercise12 {
+    public static void main(String[] args) {
+    boolean i = compare(2,3);
+    System.out.println(i);
+    }
+    public static boolean compare(int a, int b) {
+        if (a > b) return true;
+        else return false;
+    }
 }
