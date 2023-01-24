0106a0ad2d28f874b47a7cca19ef01da432dd4e8
Diego Becerra <diegofer1110@gmail.com>
2023-01-24 16:43:56 -0500

    Filter exercise done

--- src/functional/interfaces/byfunction/Exercise8.java
+++ src/functional/interfaces/byfunction/Exercise8.java
@@ -1,5 +1,8 @@
 package functional.interfaces.byfunction;
 
+import java.util.List;
+import java.util.function.Function;
+
 /*
 *  Transformation
 Create a class that transforms a list of elements from one type to another using a Function<T, R>.
@@ -7,4 +10,20 @@ Create a class that transforms a list of elements from one type to another using
 * Function implementations that perform different types of transformations.
 */
 public class Exercise8 {
+
+    public static void main(String[] args) {
+
+        List<String> myNums = List.of("1","5","9","17");
+
+        Function<String,Integer> stringToInteger = Integer::parseInt;
+
+        for(String n:myNums){
+
+            System.out.println(stringToInteger.apply(n));
+
+        }
+
+    }
+
+
 }
