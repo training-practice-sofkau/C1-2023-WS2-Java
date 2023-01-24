9d0473abdcf64bffa7c9c98683c96cbe249ddaff
William Serna <williamsernac@hotmail.com>
2023-01-24 17:42:13 -0500

    Generator done
    Random Int and String generator done

--- src/functional/interfaces/byfunction/exercise6/StringGenerator.java
+++ src/functional/interfaces/byfunction/exercise6/StringGenerator.java
@@ -1,12 +1,21 @@
 package functional.interfaces.byfunction.exercise6;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
 import java.util.function.Supplier;
 
 public class StringGenerator implements Supplier<String> {
+
+    Random random = new Random();
+    List<String> names = new ArrayList<>(Arrays.asList("Selma", "Paty", "Bart", "Homer", "Lisa"));
     @Override
     public String get() {
-        //TO-DO: The logic in order to show a random string
-
         return null;
     }
+    Supplier<String> randStr = () -> names.get(random.nextInt(names.size()));
 }
+
+
+
