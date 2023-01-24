c020a81fd96bf6f04e3b01fa260ee290ae5d668c
Julian Rojas <julianrojasgeo@gmail.com>
2023-01-24 14:04:40 -0500

    Exercise5 custom comparator for Integers and Strings
    The strings are compared according to the lenght, and Integers according to its magnidute

--- src/functional/interfaces/custom/Comparision.java
+++ src/functional/interfaces/custom/Comparision.java
@@ -4,6 +4,34 @@ public class Comparision implements CustomComparator{
     @Override
     public int comparator(Object o1, Object o2) {
         //TO-DO: Implement the neccesary logic to acomplish the assessment. Just consider String and Integer classses
-        return 0;
+        int result = 0;
+        if (o1.getClass() == o2.getClass()){
+            //Compares the length of the strings
+            if (o1 instanceof String){
+                if (((String) o1).length()==((String) o2).length() ){
+                    return 0;
+                }
+                if (((String) o1).length()>((String) o2).length() ){
+                    return 1;
+                }
+                if (((String) o1).length()<((String) o2).length() ){
+                    return -1;
+                }
+
+            }
+
+            if (o1 instanceof Integer){
+                if ((Integer)o1==(Integer)o2){
+                    return 0;
+                }
+                if ((Integer)o1>(Integer)o2){
+                    return 1;
+                }
+                if ((Integer)o1<(Integer)o2){
+                    return -1;
+                }
+            }
+        }
+        return result;
     }
 }
