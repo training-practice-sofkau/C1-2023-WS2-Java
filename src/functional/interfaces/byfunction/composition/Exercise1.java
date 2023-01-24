011eaa8aec7638dac07fc9aca73be69f28474ce8
BrianSammit <briansammit.cruz@gmail.com>
2023-01-24 17:04:04 -0500

    Finish exercise 1 concatenating the functions and applying to all members of the list

--- src/functional/interfaces/byfunction/composition/Exercise1.java
+++ src/functional/interfaces/byfunction/composition/Exercise1.java
@@ -10,23 +10,18 @@ import java.util.function.Function;
 public class Exercise1 {
     public static void main(String[] args) {
         List<Integer> numbers = List.of(2,3,5,1,7,8,9,3,2);
-
         //First function: square a number
         Function<Integer, Double> square = n -> Math.pow(n,2);
-
         //Second function: add one
         Function<Double, Double> addOne = m -> m + 1;
-
         //Last function: square root
-        Function<Double,Double> squareRoot = o -> Math.sqrt(o);
         Function<Double,Double> squareRootMR = Math::sqrt;
 
-        //TO - DO: Do the combination
-        //Hint: forEach() or for but the simply one
+        Function<Integer, Double> chain = square.andThen(addOne).andThen(squareRootMR);
+
         for(int x: numbers){
-
+           Double result = chain.apply(x);
+           System.out.println(result);
         }
-
-        //numbers.forEach();
     }
 }
