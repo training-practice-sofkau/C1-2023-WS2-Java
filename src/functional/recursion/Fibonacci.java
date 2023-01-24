4a7146bd30a2ef13ba6a18ecd13e7f9de05ac208
Diego Becerra <diegofer1110@gmail.com>
2023-01-24 12:45:57 -0500

    Fibonacci exercise done

--- src/functional/recursion/Fibonacci.java
+++ src/functional/recursion/Fibonacci.java
@@ -3,11 +3,12 @@ package functional.recursion;
 public class Fibonacci {
     //Started numbers: 0 & 1
 
-    public static int calculate(int n){
+    public static int calculate(final int n){
+
         if( n == 0 ) return 0;
         if( n == 1) return 1;
 
         //TO-DO: Implement the recursion dynamic to follow in order to show the fibonnaci serie
-        return 0;
+        return calculate(n-1) + calculate(n-2);
     }
 }
