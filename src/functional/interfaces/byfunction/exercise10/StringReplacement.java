24c5f45e5a5f46e7734a1c54c18c78b5d03d42d4
Julian Rojas <julianrojasgeo@gmail.com>
2023-01-24 17:17:02 -0500

    Exercise 10. String replacement

--- src/functional/interfaces/byfunction/exercise10/StringReplacement.java
+++ src/functional/interfaces/byfunction/exercise10/StringReplacement.java
@@ -1,2 +1,20 @@
-package functional.interfaces.byfunction.exercise10;public class StringReplacement {
+package functional.interfaces.byfunction.exercise10;
+
+import java.util.List;
+import java.util.function.BiFunction;
+import java.util.function.Function;
+
+public class StringReplacement implements BiFunction<String,String,String> {
+
+
+    @Override
+    public String apply(String s, String s2) {
+        String firstChar = s2.charAt(0)+"";
+        String secondChar = s2.charAt(1)+"";
+
+        String newString = s.replaceAll( firstChar ,secondChar );
+        return newString;
+    }
 }
+
+
