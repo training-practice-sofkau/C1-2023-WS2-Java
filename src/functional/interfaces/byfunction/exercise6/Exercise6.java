15908401a31a9405bfadd0cf375d516b1ce30d3d
Juangmcz <juangui-089@hotmail.com>
2023-01-24 16:03:05 -0500

    Exercise 6 is done.

--- src/functional/interfaces/byfunction/exercise6/Exercise6.java
+++ src/functional/interfaces/byfunction/exercise6/Exercise6.java
@@ -1,6 +1,7 @@
 package functional.interfaces.byfunction.exercise6;
 
 import java.util.function.Supplier;
+import java.util.Random;
 
 /*
 * Data Generator
@@ -11,6 +12,43 @@ Create a class that generates random data of a given type using a Supplier<Strin
 public class Exercise6 {
     public static void main(String[] args) {
 
+        System.out.println("This is a random string: " + generateString());
+        System.out.println("This is a random string: " + generateString());
+        System.out.println("This is a random string: " + generateString());
+        System.out.println("This is a random string: " + generateString());
+        System.out.println("This is a random string: " + generateString());
 
+        System.out.println("This is a random integer: " + generateInteger());
+        System.out.println("This is a random integer: " + generateInteger());
+        System.out.println("This is a random integer: " + generateInteger());
+        System.out.println("This is a random integer: " + generateInteger());
+        System.out.println("This is a random integer: " + generateInteger());
+
+    }
+
+    // Suppliers for random generations
+    public static String generateString() {
+        Supplier<String> stringGenerator = () -> randomString();
+        return stringGenerator.get();
+    }
+
+    public static Integer generateInteger() {
+        Supplier<Integer> integerGenerator = () -> randomInteger();
+        return integerGenerator.get();
+    }
+
+    // Methods for random Suppliers
+    public static Integer randomInteger() {
+
+        Random randInt = new Random();
+        randInt.setSeed(System.currentTimeMillis());
+        return randInt.nextInt(4880);
+    }
+    public static String randomString() {
+
+        Random rand = new Random();
+
+        return rand.ints(48, 123).filter(num -> (num<58 || num>64) && (num<91 || num>96))
+                .limit(15).mapToObj(c -> (char)c).collect(StringBuffer::new, StringBuffer::append, StringBuffer::append).toString();
     }
 }
