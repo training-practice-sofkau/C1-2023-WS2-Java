363f1be68e2760768c56b8c2b4194a761f4ce7cc
William Serna <williamsernac@hotmail.com>
2023-01-24 13:30:30 -0500

    Calculator finished
    Calculator exercise finished

--- src/functional/pure/Calculator.java
+++ src/functional/pure/Calculator.java
@@ -7,6 +7,8 @@ public class Calculator {
     public int add(final int x, final int y){
         return  x + y;
     }
-
+    public int subtract(final int x, final int y){return x - y;}
+    public int multiply(final int x, final int y){return x * y;}
+    public double divide(final double x, final double y){return x / y;}
     //TO-DO: The other operations
 }
