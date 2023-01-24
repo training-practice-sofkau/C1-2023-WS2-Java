ae60e4fcdc90d69ab580cc5c5faea2968aeecc4b
isan-san <isan.9.9f@gmail.com>
2023-01-24 13:21:37 -0500

    Finished Exercise1 successfully

--- src/functional/interfaces/byfunction/composition/Exercise1.java
+++ src/functional/interfaces/byfunction/composition/Exercise1.java
@@ -12,20 +12,19 @@ public class Exercise1 {
         List<Integer> numbers = List.of(2,3,5,1,7,8,9,3,2);
 
         //First function: square a number
-        Function<Integer, Double> square = n -> Math.pow(n,2);
+        Function<Double, Double> square = n -> Math.pow(n,2);
 
         //Second function: add one
         Function<Double, Double> addOne = m -> m + 1;
 
         //Last function: square root
-        Function<Double,Double> squareRoot = o -> Math.sqrt(o);
+        //Function<Double,Double> squareRoot = o -> Math.sqrt(o);
         Function<Double,Double> squareRootMR = Math::sqrt;
 
+        Function<Double, Double> integration = square.andThen(addOne).andThen(squareRootMR);
         //TO - DO: Do the combination
         //Hint: forEach() or for but the simply one
-        for(int x: numbers){
-
-        }
+        numbers.forEach(number-> System.out.println(integration.apply(Double.valueOf(number))));
 
         //numbers.forEach();
     }
