c072b7775e490fb4c57e1c1d68d84089fb3c05cb
isan-san <isan.9.9f@gmail.com>
2023-01-24 13:26:06 -0500

    Finished Exercise3 successfully

--- src/functional/recursion/Exercise3.java
+++ src/functional/recursion/Exercise3.java
@@ -5,8 +5,13 @@ package functional.recursion;
 * Use the “final” keyword to prevent reassignment of variables. Test the function by calling it with different inputs, make sure that the results are correct.
  */
 public class Exercise3 {
-    public static void main(String[] args) {
-        Fibonacci.calculate(1);
-
+    public int calculateFibonacci(int n) {
+        return n == 0 ? 0 : n == 1 ? 1 : calculateFibonacci(n - 1) + calculateFibonacci(n - 2);
     }
+
+    public static void main(String[] args) {
+        Exercise3 exercise3 = new Exercise3();
+        System.out.println(exercise3.calculateFibonacci(10));
+    }
+
 }
